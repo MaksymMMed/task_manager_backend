98896e22dc10c70b3317a73fd65d23ddf82ac830
Maksym Mediuh <maksim2003mmm15@gmail.com>
2023-02-14 16:09:38 +0200

    Добавьте файлы проекта.

---
+++ TodoBackendDAL/Entities/InnerToDo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoBackendDAL.Entities
+{
+    public class InnerToDo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int ToDoId { get; set; }
+        public ToDo ParentToDo { get; set; }
+    }
+}
---
+++ TodoBackendDAL/Entities/ToDo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoBackendDAL.Entities
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public bool IsComplete { get; set; }
+        public string? About { get; set; } 
+        public List<InnerToDo> InnerToDoList { get; set;}
+        public int CollectionId { get; set; }
+        public Collection Collection { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
